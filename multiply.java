cad93c2bbb5db67c8863f57009008038c93546b9
indiantrooper <amrit.iitkgp@gmail.com>
2024-01-17 15:23:39 +0530

    multiply

---
+++ multiply.java
@@ -0,0 +1,14 @@
+public class multiply {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        // Add the variables
+        int sum = num1 * num2;
+
+        // Print the result
+        System.out.println("The diff of " + num1 + " and " + num2 + " is " + sum + ".");
+    }
+}
+
