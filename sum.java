0dd555de9049a854ee8d876e4f500b88446b2ca3
indiantrooper <amrit.iitkgp@gmail.com>
2024-01-17 13:55:59 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class sum {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the variables
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+    }
+}
